9fb407337053993a3ed690c6946b395e5b53633c
ylafaro0310 <y16mukurou@gmail.com>
2019-05-08 00:04:08 +0900

    Add emacs and Modify chsh in setup_software.sh

--- setup_software.sh
+++ setup_software.sh
@@ -9,13 +9,13 @@ sudo apt-get install fcitx fcitx-mozc
 im-config -n fcitx
 
 sudo apt-get install chrome-gnome-shell
-sudo apt-get install git
+sudo apt-get install git emacs
 
 git clone https://github.com/ylafaro0310/dotfiles.git
 sh ~/dotfiles/setup.sh
 
 sudo apt-get install zsh
-chsh
+chsh -s /usr/bin/zsh
 
 sudo apt-get install \
     apt-transport-https \
