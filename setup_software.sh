7c1339917dd87085d6e66652d4c4a2024f815a9c
ylafaro0310 <y16mukurou@gmail.com>
2019-05-11 12:54:08 +0900

    Modify setup.sh and setup_software.sh

--- setup_software.sh
+++ setup_software.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 sudo add-apt-repository ppa:gnome-terminator
 sudo apt-get update
-sudo apt-get install terminator
-
-sudo apt-get install gnome-tweak-tool
-
-sudo apt-get install fcitx fcitx-mozc
+sudo apt-get install \
+     zsh git emacs terminator \
+     fcitx fcitx-mozc \
+     gnome-tweak-tool \
+     chrome-gnome-shell
+     
 im-config -n fcitx
-
-sudo apt-get install chrome-gnome-shell
-sudo apt-get install git emacs
 
 git clone https://github.com/ylafaro0310/dotfiles.git
 sh ~/dotfiles/setup.sh
 
-sudo apt-get install zsh
 chsh -s /usr/bin/zsh
 
+# ---------------------------------------
+# Install docker and docker-compose
+# ---------------------------------------
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
@@ -37,4 +37,7 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+# ---------------------------------------
+# Install Node.js and npm
+# ---------------------------------------
 sudo apt-get install nodejs npm
